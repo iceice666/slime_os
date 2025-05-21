d389e9ec8065a687071b447d5304e3e549785ad9
Brian Duan <iceice666@outlook.com>
2025-05-21 12:52:17 +0800

    init

---
+++ kernel/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// static HELLO: &[u8] = b"Hello World!";
+
+// #[unsafe(no_mangle)]
+// pub extern "C" fn _start() -> ! {
+//      let vga_buffer = 0xb8000 as *mut u8;
+
+//     for (i, &byte) in HELLO.iter().enumerate() {
+//         unsafe {
+//             *vga_buffer.offset(i as isize * 2) = byte;
+//             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//         }
+//     }
+
+//     loop {}
+// }
+
+bootloader_api::entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    loop {}
+}
